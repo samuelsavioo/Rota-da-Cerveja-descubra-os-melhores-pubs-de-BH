9e5da97e100ae776d8c363df3bb641bd16c0df08
samuelsavioo <samueelsavio@gmail.com>
2024-09-06 12:15:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,55 +1,47 @@
+function pesquisar() {
+  
+  let section = document.getElementById("resultados-pesquisa");
 
- function pesquisar () {
-  let section = document.getElementById("resultados-pesquisa")
+  let campoPesquisa = document.getElementById("campo-pesquisa").value
 
-  let campoPesquisa = document.getElementById("campo-pesquisa").value 
-
+  
   if (!campoPesquisa) {
-    section.innerHTML =  "<p>Nada foi encontrado. Você precisa digitar o nome de algum bar ou o que deseja encontrar</p>"
+      section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de algum bar ou o que deseja encontrar</p>"
+      return 
   }
 
   campoPesquisa = campoPesquisa.toLowerCase()
 
-  let resultados = ""
-  let titulo = ""
-  let descricao = ""
-  let tags = ""
+  
+  let resultados = "";
+  let titulo = ""; 
+  let descricao = "";
+  let tags = "";
 
-    for(let dado of dados) {
-
+  
+  for (let dado of dados) {
       titulo = dado.titulo.toLowerCase()
       descricao = dado.descricao.toLowerCase()
       tags = dado.tags.toLowerCase()
-
-     if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)
-    ){
-      resultados += 
-      `<div class="item-resultado">
       
-     <h2>
-        ${dado.titulo}
-     </h2> 
-   
-     <p 
-     class="descricao-meta"> ${dado.descricao}
-     </p>
-   
-     <a
-      href="${dado.link}">Mais informações
-      </a>
-   
-   </div>`; 
-      
+      if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+          
+          resultados += `
+          <div class="item-resultado">
+              <h2>
+                  <a href="#" target="_blank">${dado.titulo}</a>
+              </h2>
+              <p class="descricao-meta">${dado.descricao}</p>
+              <a href=${dado.link} target="_blank">Mais informações</a>
+          </div>
+      `;
       }
-
-     }
-       
-  if (!resultados) {
-       resultados = "<p>Poxaa, não encontramos o seu bar</p>"
-
   }
 
-  section.innerHTML = resultados;
+  if (!resultados) {
+      resultados = "<p>Poxaa, não encontramos o seu bar</p>"
+  }
 
-}
   
+  section.innerHTML = resultados;
+}
